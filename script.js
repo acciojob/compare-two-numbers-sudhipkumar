8aa9cf2f374f6d726f358e97b35c1f80d50e3fcf
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-18 13:11:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let num1=prompt("Enter Number One");
+let num2=prompt("Enter Number Two");
+
+
+if(num1===num2){
+	alert(true);
+}
+else if(typeof num1 == typeof num2){
+	alert("Please Enter Numbers");
+}
+else{
+	alert(false);
+}
